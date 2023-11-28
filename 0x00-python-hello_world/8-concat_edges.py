77ad77487813bf99db7dbb5663dc560b14f66d11
root <root@VICTORY-PC>
2023-11-28 05:27:30 +0100

    all python mandatory task done

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interepreted, interactive, object-oriented programming\
+       language that combines remarkable power with very clear syntax"
+str = str[40:67] + str[-23:-17] + str[0:6]
+print(str)
