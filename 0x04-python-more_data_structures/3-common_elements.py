844203ca486270408ffe0bc6a95f43704b99664e
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-01-08 16:36:21 +0100

    hmm

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def common_elements(set_1, set_2):
+    """Returns common elements"""
+
+    set_3 = set([x for x, y in zip(set_1, set_2) if x == y])
+    return set_3
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# #!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    number_of_keys = len(a_dictionary.keys())
+    return number_of_keys
