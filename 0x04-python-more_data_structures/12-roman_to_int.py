5dc7a5916524eb641d7278b5809915f9fd50a146
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-01-08 17:33:07 +0100

    many undone task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    num = 0
+    if (type(roman_string) is not str) or (type(roman_string) is None):
+        return 0
+
+    for i, c in enumerate(roman_string):
+        if roman[c]:
+            num += roman[c]
+            if roman[c] > roman[roman_string[i - 1]] and i > 0:
+                num -= 2 * roman[roman_string[i - 1]]
+        else:
+            return 0
+    return num
