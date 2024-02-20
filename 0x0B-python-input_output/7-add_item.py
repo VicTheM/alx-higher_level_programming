974b278b1bc871a89b6f76fddb8ec095cd297d73
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-02-20 19:32:33 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,6 @@ passed to it in json format in a json file
 """
 
 import sys
-import json
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
 
