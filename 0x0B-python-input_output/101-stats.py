e1d609350d9ccbf7ac973c50803fe1bd83f5d705
VicTheM <117105067+VicTheM@users.noreply.github.com>
2024-02-29 03:44:26 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,103 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
 """
+Script that reads from stdin line by line and computes metrics
+"""
+import sys
+import datetime
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+def verify_line(line):
     """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    This function verifies that the input format of the
+    lines are correct before computing metrics
+
+    Args:
+        line(str): String containg the line input to be verified
+
+    Returns:
+        True if line is valid else False
+    """
+
+    f_dash = line.find('-')
+    if (f_dash == -1):
+        return
+    maybe_ip = line[:f_dash].split()[0]
+
+    l_a = line[f_dash + 1:].split()
+    l_a.insert(0, '-')
+    l_a.insert(0, maybe_ip)
+
+    if len(l_a) != 9:
+        return False
+
+    get_str = '"GET /projects/260 HTTP/1.1"'
+    c_ip = l_a[0].split('.')
+    if len(c_ip) != 4 and all(x.isdigit() for x in c_ip):
+        return False
+    elif l_a[1] != '-':
+        return False
+    try:
+        datetime.datetime.fromisoformat(((f"{l_a[2]} {l_a[3]}")[1:-1]))
+    except ValueError:
+        return False
+
+    if f"{l_a[4]} {l_a[5]} {l_a[6]}" != get_str:
+        return False
+    elif not l_a[8].rstrip('\n').isdigit():
+        return False
+
+    return True
 
 
-if __name__ == "__main__":
-    import sys
+def compute_prnt_metrics(all_lines):
+    """
+    This function computes and prints the metrics to stdout
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    Args:
+        all_lines(list): ALl lines read so far from stdin
+    """
+    dict_status = {'200': 0, '301': 0, '400': 0,
+                   '401': 0, '403': 0, '404': 0,
+                   '405': 0, '500': 0}
+    file_size = 0
+    tmp_str = ""
 
+    for line in all_lines:
+        if not verify_line(line):
+            continue
+        split_line = line.split(' ')
+        file_size += int(split_line[-1][0:-1])
+        if split_line[-2] in dict_status.keys():
+            dict_status[split_line[-2]] += 1
+
+    for key in sorted(dict_status.keys()):
+        if dict_status[key]:
+            tmp_str += "{}: {}\n".format(key, dict_status[key])
+
+    sys.stdout.write("File size: {}\n".format(file_size))
+    sys.stdout.write(tmp_str)
+    sys.stdout.flush()
+
+
+def main():
+    """
+    This program serves as an entry point to the script
+
+    It continually reads lines from stdin and stores them in
+    a list then passes that list to the compute function
+    when certain requirements are met
+    """
+    all_lines = []
     try:
         for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
+            all_lines.append(line)
+            if len(all_lines) and len(all_lines) % 10 == 0:
+                compute_prnt_metrics(all_lines)
+                continue
+        compute_prnt_metrics(all_lines)
+    except KeyboardInterrupt as e:
+        compute_prnt_metrics(all_lines)
+        raise e
 
-            line = line.split()
 
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+main()
