4509b39166c7587483eb39ceb980536566650d74
VicTheM <117105067+VicTheM@users.noreply.github.com>
2024-02-29 03:29:46 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
-"""
-This program adds all arguments in argv to a Python list and saves
-them all to a file.
-
-"""
-import json
-import os
+"""A script that adds all arguments to a Python list,
+    and then save them to a file."""
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-def main():
-    """
-    Entry point to program
-    """
-    file = "add_item.json"
-    just_created = 0 if os.path.exists(file) else 1
-    tmp_list = []
-
-    with open(file, f"{'w' if just_created else 'r+'}", encoding="utf-8") as f:
-        if not just_created:
-            list_items = json.load(f)
-            tmp_list.extend(list_items)
-            f.seek(0)
-            f.truncate(0)
-
-        tmp_list.extend(sys.argv[1:])
-        json.dump(tmp_list, f)
-
-
-main()
+filename = "add_item.json"
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
