2ab37b8a6ab8b83577ba925552c3f78b90c164a6
VicTheM <117105067+VicTheM@users.noreply.github.com>
2024-02-29 03:25:13 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,13 +2,16 @@
 """ Custom JSON Serializer """
 
 
-
 def class_to_json(obj):
     """
     Returns the dict representation of obj
     so it can be serialized by json
 
+    Args:
+        obj: (any)
+
     NOTE: all attributes of obj must be
     inherently serializable
     """
+    
     return obj.__dict__
