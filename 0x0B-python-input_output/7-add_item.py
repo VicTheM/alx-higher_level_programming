5e67ca7f3d0582deba2a56a58ba5159e3bd09422
VicTheM <117105067+VicTheM@users.noreply.github.com>
2024-02-29 03:28:07 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
-''' function that writes an Object to a text file, using a JSON representation
-'''
+"""
+This program adds all arguments in argv to a Python list and saves
+them all to a file.
+
+"""
 import json
+import os
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def save_to_json_file(my_obj, filename):
-    ''' module save_to_json_file
-    accepts Python object and sends JSON representation to file
-    '''
-    with open(filename, 'w') as f:
-        f.write(json.dumps(my_obj))
+def main():
+    """
+    Entry point to program
+    """
+    file = "add_item.json"
+    just_created = 0 if os.path.exists(file) else 1
+    tmp_list = []
+
+    with open(file, f"{'w' if just_created else 'r+'}", encoding="utf-8") as f:
+        if not just_created:
+            list_items = json.load(f)
+            tmp_list.extend(list_items)
+            f.seek(0)
+            f.truncate(0)
+
+        tmp_list.extend(sys.argv[1:])
+        json.dump(tmp_list, f)
+
+
+main()
