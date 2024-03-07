3e27cf29831d43f9f36c197bcd1902ec6bb87b6f
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-03-07 20:20:29 +0100

    Done

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+/* Multiple queries */
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,5 @@
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+WHERE value IS NOT NULL
+GROUP BY state
+ORDER BY state ASC;
