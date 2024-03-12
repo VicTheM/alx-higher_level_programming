787022291bdf825ce2d56b282dc127a60a471b4e
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-03-12 19:06:18 +0100

    Done with all

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 // Reads command line argumemts
 
-let args = process.argv;
-let len = args.length;
+const args = process.argv
+const len = args.length
 if (len <= 2) {
-	console.log(`No argument`);
-}
-else if (len === 3) {
-	console.log(`Argument found`);
-}
-else {
-	console.log(`Arguments found`);
+  console.log('No argument')
+} else if (len === 3) {
+  console.log('Argument found')
+} else {
+  console.log('Arguments found')
 }
