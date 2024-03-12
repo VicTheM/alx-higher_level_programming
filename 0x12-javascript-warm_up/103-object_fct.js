d9113caea305df0bffe8f710afe70aa7604fee7b
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-03-13 00:29:14 +0100

    Changed from anonymous function

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const myObject = {
   type: 'object',
   value: 12
 }
 console.log(myObject)
 
-myObject.incr = function () { this.value++ }
+myObject.incr = function myFunc() { this.value++ }
 
 myObject.incr()
 console.log(myObject)
