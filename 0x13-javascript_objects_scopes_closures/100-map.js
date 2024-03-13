2abd1591681346947f5bfcacc4eef41381cfdc6b
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-03-13 03:38:21 +0100

    Done

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This script imports an array and computes a new array
+
+const list = require('./100-data').list;
+const newList = list.map((index, value) => index * value);
+
+console.log(list);
+console.log(newList);
