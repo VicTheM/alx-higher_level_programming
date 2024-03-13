2234934a5de8fdc232a6251013bc82f255d6bf23
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-03-13 02:44:20 +0100

    Semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,21 +4,21 @@
 class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-      this.width = w
-      this.height = h
+      this.width = w;
+      this.height = h;
     }
   }
 
   // Public instance method
   print () {
-    let x
-    let output = ''
-  	for (x = 0; x < this.width; x++) {
-    	  output += 'X'
-  	}
- 	for (x = 0; x < this.height; x++) {
-    	  console.log(output)
+    let x;
+    let output = '';
+    for (x = 0; x < this.width; x++) {
+      output += 'X';
+    }
+    for (x = 0; x < this.height; x++) {
+      console.log(output);
     }
   }
 }
-module.exports = Rectangle
+module.exports = Rectangle;
