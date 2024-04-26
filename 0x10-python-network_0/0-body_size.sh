9785e77fcc738dc08d34c7b9537be8b03c137878
Victory <victorychibuike121@gmail.com>
2024-04-26 05:11:50 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,8 @@
 # This script displays the size of a Http response from a server
 # It receives an IP to curl as parameter, port could also be specified
 
-response=$(curl -s $1)
-echo "${#response}"
+#response=$(curl -s $1)
+#echo "${#response}"
+
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
