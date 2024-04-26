8b84c9e6644b9fa1cc9b5fc033e85542e5c7085a
Victory <victorychibuike121@gmail.com>
2024-04-26 05:16:15 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
-# This script sends GET
-
+#!/bin/bash
+# This script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s -H "X-School-User-Id: 98" "$1"
 # send data and display body of response
-curl -X GET --data "X-School-User-Id=98" $1
+# curl -X GET --data "X-School-User-Id=98" $1
 
