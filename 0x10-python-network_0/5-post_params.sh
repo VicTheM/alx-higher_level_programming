1c7b7316b9ec473bf5f3d97ae42227ef766cccbf
Victory <victorychibuike121@gmail.com>
2024-04-26 05:51:31 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-# curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
-
-# send data and display body of response
-curl -X POST --data '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' --header 'ContentType: application/json' $1
-
+curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
