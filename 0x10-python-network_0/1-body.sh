4e36e5b891d245a86ad5de724d4470badc1d3da2
Victory <victorychibuike121@gmail.com>
2024-04-26 05:50:31 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s -L "$1"
-
-# get header + body
-## curl -X GET -s -i $1 --output /tmp/tmp_curl
-## head --lines=1 /tmp/tmp_curl | grep --only-matching 200
-
-# If the response is 200, output the body of response
-# there is always a blank line before the message boddy
-## if [ $? -eq 0 ]; then
-##	grep -x -m 1 -A 10000 /tmp/tmp_curl | tail --lines=+2
-## fi
-## rm /tmp/tmp_curl
