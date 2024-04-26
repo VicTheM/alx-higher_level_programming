ec1219c33b4ad69c497d18d9d945732689c7fced
Victory <victorychibuike121@gmail.com>
2024-04-26 05:13:53 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,1 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
+# This script sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -s -o /dev/null -w "%{response_code}" "$1"
