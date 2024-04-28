058aa00446049fd3ebed05efb44f355c0ff79e70
Victory <victorychibuike121@gmail.com>
2024-04-28 14:37:20 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     values = {
-        "email": email,
+        "email": email
     }
 
     res = requets.post(url, data=values)
