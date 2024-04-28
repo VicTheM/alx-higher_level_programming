73161ec2c0ef9c0b81c96fef2b85da29132979e0
Victory Chibuike <victorychibuike121@gmail.com>
2024-04-28 13:39:15 +0100

    untested code

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Posts an email to a server with the email address as parameter."""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {"email": email}
+
+    res = requets.post(url, data=values)
+        print(res.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Make a HTTP request and prints the status code if either a client or server
+error occurs"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print("\t- content: {}".format(req.text))
