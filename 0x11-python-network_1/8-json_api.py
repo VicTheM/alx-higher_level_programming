16d9e5a4eae6a4c4644c157cd941a2dfc00dec7f
Victory Chibuike <victorychibuike121@gmail.com>
2024-04-28 14:31:00 +0100

    Untested

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""This script demonstrates the simple use of an API.
+It makes a request to an external application and if the
+returned response is JSON formatted, it searches for the
+required data in the response"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv >= 2):
+        sch_str = sys.argv[1]
+    else:
+        sch_str = ""
+
+    payload = {'q': sch_str}
+    req = requsts.post(url, data=payload)
+
+    try:
+        if req.json == {}:
+            print ("No result")
+        else:
+            print("[{}] {}".format(req.json()["id"], req.json["name"]))
+    except ValueError:
+        print("Not a valid JSON")
